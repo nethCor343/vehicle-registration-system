56e90065ac4b52361938b0baaa45418091ff63b5
Neth Corrales <nethcordev343@gmail.com>
2025-11-20 19:59:12 -0400

    feat: create Person class with identification attributes

---
+++ src/main/java/vehiclesystem/model/Person.java
@@ -0,0 +1,12 @@
+package vehiclesystem.model;
+
+public class Person {
+    private String firstName;
+    private String paternalSurname;
+    private String maternalSurname;
+    private String idNumber;
+
+    public Person(String firstName, String paternalSurname, String maternalSurname, String idNumber) {
+        
+    }
+}
