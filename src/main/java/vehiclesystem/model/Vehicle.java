07e0027c3fef35341c5bc2dfd8cb77182c06f65f
Neth Corrales <nethcordev343@gmail.com>
2025-11-20 17:06:07 -0400

    feat: add remaining vehicle fields and getters

--- src/main/java/vehiclesystem/model/Vehicle.java
+++ src/main/java/vehiclesystem/model/Vehicle.java
@@ -6,6 +6,13 @@ public abstract class Vehicle {
     public static final int PLATE_LETTERS = 3;
     public static final int PLATE_TOTAL_LENGTH = PLATE_DIGITS + PLATE_LETTERS;
 
+    private static final int BRAND_MIN_LEN = 2;
+    private static final int BRAND_MAX_LEN = 30;
+    private static final int MODEL_MIN_LEN = 1;
+    private static final int MODEL_MAX_LEN = 20;
+    private static final int COLOR_MIN_LEN = 3;
+    private static final int COLOR_MAX_LEN = 15;
+
     private String plate;
     private String brand;
     private String model;
@@ -13,6 +20,9 @@ public abstract class Vehicle {
     
     public Vehicle(String plate, String brand, String model, String color) {
         setPlate(plate);
+        setBrand(brand);
+        setModel(model);
+        setColor(color);
     }
 
     private void setPlate(String plate) {
@@ -75,8 +85,32 @@ public abstract class Vehicle {
         return text;
     }
 
+    private void setBrand(String brand) {
+        this.brand = validateText(BRAND_MIN_LEN, BRAND_MAX_LEN, brand, "Brand");
+    }
+
+    private void setModel(String model) {
+        this.model = validateText(MODEL_MIN_LEN, MODEL_MAX_LEN, model, "Model");
+    }
+
+    private void setColor(String color) {
+        this.color = validateText(COLOR_MIN_LEN, COLOR_MAX_LEN, color,"Color");
+    }
+
     public String getPlate() {
         return plate;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
     }
 
 }
