447e3c4ec07ee975cfd0feedac94a52558ca79b0
Neth Corrales <nethcordev343@gmail.com>
2025-11-20 19:32:49 -0400

    feat: validate ranges for horsePower and cargoCapacity and implement getters

---
+++ src/main/java/vehiclesystem/model/Truck.java
@@ -0,0 +1,46 @@
+package vehiclesystem.model;
+
+public class Truck  extends Vehicle {
+
+    private static final int MIN_HP = 90;
+    private static final int MAX_HP = 450;
+    private static final double MIN_CAPACITY = 0.5;
+    private static final double MAX_CAPACITY = 400.0;
+
+    private int horsePower;
+    private double cargoCapacity;
+
+    public Truck(String plate, String brand, String model, String color, int horsePower, double cargoCapacity) {
+        super(plate, brand, model, color);
+        setHorsePower(horsePower);
+        setCargoCapacity(cargoCapacity);
+    }
+
+    private void setHorsePower(int horsePower) {
+        if (horsePower < MIN_HP || horsePower > MAX_HP) {
+            throw new IllegalArgumentException(
+                "HorsePower (" + horsePower + ") must be between " + MIN_HP + " and " + MAX_HP + "."
+            );
+        }
+        this.horsePower = horsePower;
+    }
+
+    private void setCargoCapacity(double cargoCapacity) {        
+        if (cargoCapacity < MIN_CAPACITY || cargoCapacity > MAX_CAPACITY) {
+            throw new IllegalArgumentException(
+                "Cargo capacity (" + cargoCapacity + ") must be between " + MIN_CAPACITY +
+                " and " + MAX_CAPACITY + " tons."
+            );
+        }
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public double getCargoCapacity() {
+        return cargoCapacity;
+    }
+    
+}
